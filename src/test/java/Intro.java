b14ac0beade319dae1f4caa387574a0526682bfb
Krzysztof Chruściel <kchrusciel@users.noreply.github.com>
2020-04-21 09:02:16 +0200

    Update Intro

--- src/test/java/Intro.java
+++ src/test/java/Intro.java
@@ -1,9 +1,9 @@
 class Intro {
 
-    // In Polish on my blog
+    // In Polish on our blog
     // https://codecouple.pl/2018/04/06/completablefuture-dlaczego-po-co-jak/
 
-    // In English on my friend's blog
+    // In English on Tomasz Nurkiewicz blog
     // https://www.nurkiewicz.com/2013/05/java-8-definitive-guide-to.html
 
     //CompletableFuture
