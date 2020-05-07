5dd6819e812601a4421fd10e718adb2fc4baf70a
Krzysztof Chrusciel <krzysztof.chrusciel@jamf.com>
2020-05-07 11:28:53 +0200

    Update Future

--- src/test/java/S01_Future.java
+++ src/test/java/S01_Future.java
@@ -1,10 +1,16 @@
-import com.google.common.util.concurrent.ThreadFactoryBuilder;
-import org.junit.jupiter.api.DisplayName;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.TimeUnit;
+
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.concurrent.*;
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
 
 /**
  * 2004
@@ -12,86 +18,85 @@ import java.util.concurrent.*;
  */
 class S01_Future {
 
-    private static final Logger log = LoggerFactory.getLogger(S01_Future.class);
+	private static final Logger log = LoggerFactory.getLogger(S01_Future.class);
 
-    @Test
-    public void sjug_7() throws Exception {
-        MyTask myTask = new MyTask();
-        myTask.call();
+	@Test
+	public void howToRun() throws Exception {
+		MyTask myTask = new MyTask();
+		myTask.call();
 
-        //How to run?
-    }
+		//How to run?
+	}
 
-    @Test
-    public void sjug_21() throws Exception {
-        MyTask myTask = new MyTask();
+	@Test
+	public void executorService() throws Exception {
+		MyTask myTask = new MyTask();
 
-        //ExecutorService
-        ExecutorService executorService = Executors.newFixedThreadPool(10);
-        Future<String> callable = executorService.submit(myTask);
-        callable.get(); //blocking
-    }
+		//ExecutorService
+		ExecutorService executorService = Executors.newFixedThreadPool(10);
+		Future<String> callable = executorService.submit(myTask);
+		callable.get(); //blocking
+	}
 
-    @Test
-    @DisplayName("ThreadFactory")
-    public void sjug_34() throws Exception {
-        MyTask myTask = new MyTask();
+	@Test
+	public void threadFactory() throws Exception {
+		MyTask myTask = new MyTask();
 
-        ThreadFactory threadFactory = new ThreadFactoryBuilder()
-                .setDaemon(true)
-                .setNameFormat("import-task-%d")
-                .build();
+		ThreadFactory threadFactory = new ThreadFactoryBuilder()
+				.setDaemon(true)
+				.setNameFormat("import-task-%d")
+				.build();
 
-        //pool-n-thread-m
+		//pool-n-thread-m
 
-        //ExecutorService
-        ExecutorService executorService = Executors.newFixedThreadPool(10, threadFactory);
-        Future<String> callable = executorService.submit(myTask);
-        callable.get(); //blocking
-    }
+		//ExecutorService
+		ExecutorService executorService = Executors.newFixedThreadPool(10, threadFactory);
+		Future<String> callable = executorService.submit(myTask);
+		callable.get(); //blocking
+		callable.get(5, TimeUnit.SECONDS); //blocking
+	}
 
-    @Test
-    public void sjug_51() throws Exception {
-        MyTask resultFromFirstDB = new MyTask();
-        MyTask doSomethingOnResultFromDB = new MyTask();
+	@Test
+	public void twoResults() throws ExecutionException, InterruptedException {
+		MyTask resultFromFirstDB = new MyTask();
+		MyTask doSomethingOnResultFromDB = new MyTask();
 
-        ExecutorService executorService = Executors.newFixedThreadPool(10);
-        Future<String> firstResult = executorService.submit(resultFromFirstDB);
+		ExecutorService executorService = Executors.newFixedThreadPool(10);
+		Future<String> firstResult = executorService.submit(resultFromFirstDB);
 
-        String resultFromFirstFuture = firstResult.get(); //blocking
+		String resultFromFirstFuture = firstResult.get(); //blocking
 
-        Future<String> secondResult = executorService.submit(doSomethingOnResultFromDB);
+		Future<String> secondResult = executorService.submit(doSomethingOnResultFromDB);
 
-        String resultFromSecondFuture = secondResult.get(); //blocking
+		String resultFromSecondFuture = secondResult.get(); //blocking
 
-        //How to compose transformations?
-    }
+		//How to compose transformations?
+	}
 
-    @Test
-    public void sjug_66() throws Exception {
-        MyTask resultFromFirstDB = new MyTask();
-        MyTask resultFromSecondDB = new MyTask();
+	@Test
+	public void firstResult() {
+		MyTask resultFromFirstDB = new MyTask();
+		MyTask resultFromSecondDB = new MyTask();
 
-        ExecutorService executorService = Executors.newFixedThreadPool(10);
-        Future<String> firstResult = executorService.submit(resultFromFirstDB);
-        Future<String> secondResult = executorService.submit(resultFromSecondDB);
+		ExecutorService executorService = Executors.newFixedThreadPool(10);
+		Future<String> firstResult = executorService.submit(resultFromFirstDB);
+		Future<String> secondResult = executorService.submit(resultFromSecondDB);
 
-//        String resultFromFirstFuture = firstResult.get(); //blocking
-//        String resultFromSecondFuture = secondResult.get(); //blocking
+		//        String resultFromFirstFuture = firstResult.get(); //blocking
+		//        String resultFromSecondFuture = secondResult.get(); //blocking
 
-
-        //How to get first result?
-    }
+		//How to get first result?
+	}
 
 }
 
 class MyTask implements Callable<String> {
 
-    private static final Logger log = LoggerFactory.getLogger(MyTask.class);
+	private static final Logger log = LoggerFactory.getLogger(MyTask.class);
 
-    @Override
-    public String call() throws Exception {
-        log.info("Callable");
-        return "Callable";
-    }
+	@Override
+	public String call() {
+		log.info("Callable");
+		return "Callable";
+	}
 }
